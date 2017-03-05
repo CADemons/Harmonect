dd5c5865375d6f7d3fc76018ebe85e790229d296
LincB <LincB@users.noreply.github.com>
2017-03-05 11:08:28 -0500

    Add test data and command line argument for depth_import

--- depth_import.py
+++ depth_import.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import sys
 import freenect
 import cv2
 import numpy as np
 
 cv2.namedWindow('Depth')
-depth = np.loadtxt('data.csv', dtype=np.dtype('u1'), delimiter=',')
+depth = np.loadtxt(sys.argv[1], dtype=np.dtype('u1'), delimiter=',')
 while 1:
     cv2.imshow('Depth', depth)
     if cv2.waitKey(100) == 27:
