560431a8aecad4a68db5158437e52d89e9a9f0d7
LincB <LincB@users.noreply.github.com>
2017-04-09 12:01:48 -0400

    Tweaks for working step detection

--- auto_calib.py
+++ auto_calib.py
@@ -5,12 +5,10 @@ import ms_tools
 
 
 def getMasks(baseDepth, numStairs, depthThresh, dsRate, filterPx):
-    height = int(480 / dsRate)
-    width = int(640 / dsRate)
-    masks = np.zeroes((numStairs, height, width), np.dtype(bool))
-    maskImg = np.zeroes((height, width), np.dtype('uint8'))
+    masks = np.zeros((numStairs,) + baseDepth.shape, np.dtype(bool))
+    maskImg = np.zeros(baseDepth.shape, np.dtype('uint8'))
     plt.figure()
-    plt.imshow(maskImg)
+    plt.imshow(maskImg, cmap=plt.cm.gray)
     plt.show(block=False)
     for stairNum in range(1, numStairs + 1):
         raw_input('Press enter to record stair ' + str(stairNum))
@@ -19,5 +17,6 @@ def getMasks(baseDepth, numStairs, depthThresh, dsRate, filterPx):
         filteredChanged = ms_tools.thinLineFilter(changed, filterPx)
         masks[stairNum - 1] = filteredChanged
         maskImg = np.where(filteredChanged, stairNum, maskImg)
-        plt.imshow(maskImg)
+        plt.imshow(maskImg, cmap=plt.cm.gray)
+        plt.draw()
     return masks
--- musical_stairs.py
+++ musical_stairs.py
@@ -1,26 +1,29 @@
 import os
 import sys
+import time
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 
 import ms_tools
 import auto_calib
 import step_detect
-import music
+# import music
 
 
 if __name__ == '__main__':
     if os.geteuid() != 0:
         sys.exit('Error: This program must be run as root')
-    if len(sys.argv) != 6:
-        sys.exit('Usage: musical_stairs.py numStairs depthThreshold downsampleRate waitMs filterPx')
+    if len(sys.argv) != 7:
+        sys.exit('Usage: musical_stairs.py numStairs depthThreshold downsampleRate waitMs filterPx coverageThresh')
     numStairs = int(sys.argv[1])
     depthThresh = int(sys.argv[2])
     dsRate = int(sys.argv[3])
     waitMs = int(sys.argv[4])
     filterPx = int(sys.argv[5])
+    coverThresh = float(sys.argv[6])
 
+    base = ms_tools.getDepth(dsRate)
+    time.sleep(.5)
     base = ms_tools.getDepth(dsRate)
     plt.figure()
     plt.imshow(base, cmap=plt.cm.gray)
@@ -28,16 +31,16 @@ if __name__ == '__main__':
 
     masks = auto_calib.getMasks(base, numStairs, depthThresh, dsRate, filterPx)
     spaceHeld = False
+    cv2.namedWindow('Depth')
+    print('Press ESC in window to stop')
     while 1:
-        cv2.namedWindow('Depth')
-        print('Press ESC in window to stop')
         depth = ms_tools.getDepth(dsRate)
 
-        steps, vid = step_detect.getStepArr(masks, base, depth, depthThresh, filterPx)
+        steps, vid = step_detect.getStepArr(masks, base, depth, depthThresh, filterPx, coverThresh)
         cv2.imshow('Depth', vid)
         key = cv2.waitKey(waitMs)
 
-        music.processSteps(steps)
+        # music.processSteps(steps)
 
         if key == ord(' ') and not spaceHeld:
             print 'Base reset'
