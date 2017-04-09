89d90df9d48cce819ab9b82c8b89cec6b0860e82
LincB <LincB@users.noreply.github.com>
2017-04-09 10:39:54 -0400

    Add main file musical_stairs.py with three primary function calls that need to be implemented

---
+++ musical_stairs.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import freenect
+import frame_convert2
+
+import auto_calib
+import step_detect
+import music
+
+
+def getDepth(dsRate):
+    return frame_convert2.pretty_depth_cv(freenect.sync_get_depth()[0])[::dsRate, ::dsRate]
+
+
+if __name__ == '__main__':
+    if os.geteuid() != 0:
+        sys.exit('Error: This program must be run as root')
+    if len(sys.argv) != 6:
+        sys.exit('Usage: musical_stairs.py numStairs depthThreshold downsampleRate waitMs filterPx')
+    numStairs = int(sys.argv[1])
+    depthThresh = int(sys.argv[2])
+    dsRate = int(sys.argv[3])
+    waitMs = int(sys.argv[4])
+    filterPx = int(sys.argv[5])
+
+    base = getDepth(dsRate)
+    plt.figure()
+    plt.imshow(base, cmap=plt.cm.gray)
+    plt.show(block=False)
+
+    masks = auto_calib.getMasks(base, numStairs, depthThresh, dsRate, filterPx)
+    spaceHeld = False
+    while 1:
+        cv2.namedWindow('Depth')
+        print('Press ESC in window to stop')
+        depth = getDepth(dsRate)
+        diff = base - depth
+        vid = np.where(diff > depthThresh, 255, 0).astype(np.dtype('uint8'))
+        cv2.imshow('Depth', vid)
+        key = cv2.waitKey(waitMs)
+
+        steps, vid = step_detect.getStepArr(masks, base, depth, depthThresh, filterPx)
+        music.processSteps(steps)
+
+        if key == ord(' ') and not spaceHeld:
+            print 'Base reset'
+            base = depth
+            spaceHeld = True
+        else:
+            spaceHeld = False
+        if key == 27:  # ESC
+            break
