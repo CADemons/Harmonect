efb19a9964d4f7bd1a657e80b5a3958616199be5
jerry liang <jerryliang@jerrys-MacBook-Air.local>
2017-04-30 11:36:52 -0400

    implemented new detection mechanism. Still need to test.

--- auto_calib.py
+++ auto_calib.py
@@ -17,7 +17,12 @@ def getMasks(baseDepth, numStairs, depthThresh, dsRate, filterPx):
         depth = ms_tools.getDepth(dsRate)
         changed = (baseDepth - depth) > depthThresh
         filteredChanged = ms_tools.thinLineFilter(changed, filterPx)
-        masks[stairNum - 1] = filteredChanged
+        # Find average depth value of DEPTH where filteredChange is true
+        depth_region = np.where(filteredChanged, depth, 0)
+        average_depth = np.sum(depth_region)/np.sum(filteredChaged)
+
+        # Stores 2d region with average depth
+        masks[stairNum - 1] = [filteredChanged, average_depth]
         maskImg = np.where(filteredChanged, stairNum, maskImg)
         plt.imshow(maskImg, cmap=plt.cm.gray)
         plt.draw()
--- step_detect.py
+++ step_detect.py
@@ -2,13 +2,11 @@ import numpy as np
 
 import ms_tools
 
-MAX_CUTOFF = 255
-MIN_CUTOFF = 0
+# Uncertainty around average depth
+UNCERT = 10
 
 def getStepArr(masks, base, depth, depthThresh, filterPx, coverThresh):
-    # Filters out values in depth image that are outside of defined depth range
-    depth = np.where(np.logical_and(depth > MIN_CUTOFF, depth < MAX_CUTOFF), depth)
-    
+
     changed = (base - depth) > depthThresh
     filteredChanged = ms_tools.thinLineFilter(changed, filterPx)
     vid = np.where(filteredChanged, 150, 0)
@@ -16,11 +14,19 @@ def getStepArr(masks, base, depth, depthThresh, filterPx, coverThresh):
     numStairs = masks.shape[0]
     activeStairs = np.zeros(numStairs, np.dtype(bool))
     for stairNum in range(0, numStairs):
-        mask = masks[stairNum]
+        # Retrieving region
+        mask = masks[stairNum, 0]
+
+        # Retrieving Average depth value
+        maskAvgDepth = masks[stairNum, 1]
+        
         area = np.sum(mask)
         coverageArr = np.logical_and(mask, filteredChanged)
+        coveredDepths = np.where(coverageArr, depth, 0)
+        avgCoveredDepth = np.sum(coveredDepths) / np.sum(coverageArr)
+        
         covered = np.sum(coverageArr)
-        if (float(covered) / float(area)) >= coverThresh:
+        if (float(covered) / float(area)) >= coverThresh and avgCoveredDepth > maskAvgDepth - UNCERT and avgCoveredDepth < maskAvgDepth + UNCERT:
             activeStairs[stairNum] = True
             vid = np.where(mask, 255, vid)
         else:
