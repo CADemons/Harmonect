1e36caf547ad9a119fe7bde7e99ff1cd2a6ddcc6
LincB <LincB@users.noreply.github.com>
2017-05-21 11:57:40 -0400

    Add comments for calibration and step detection

--- auto_calib.py
+++ auto_calib.py
@@ -5,17 +5,27 @@ import ms_tools
 
 
 def getMasks(baseDepth, numStairs, depthThresh, dsRate, filterPx):
+    # A "mask" is an image-size 2d boolean array for each stair,
+    # true where the pixel location is part of the region triggering
+    # the stair. `masks` is an array of these masks, one for each stair.
     masks = np.zeros((numStairs,) + baseDepth.shape, np.dtype(bool))
+    # Image of masks for display during calibration
     maskImg = np.zeros(baseDepth.shape, np.dtype('uint8'))
     plt.figure()
+    # maskImg is blank for this display
     plt.imshow(maskImg, cmap=plt.cm.gray)
     plt.show(block=False)
     for stairNum in range(1, numStairs + 1):
         raw_input('Press enter to record stair ' + str(stairNum))
         depth = ms_tools.getDepth(dsRate)
+        # Pixels where depth has changed since the base image
+        # (where people are standing)
         changed = (baseDepth - depth) > depthThresh
+        # Remove thin-line noise from `changed`
         filteredChanged = ms_tools.thinLineFilter(changed, filterPx)
+        # Set the mask for each stair to its filteredChanged calibration image
         masks[stairNum - 1] = filteredChanged
+        # Overlay mask on maskImg and display
         maskImg = np.where(filteredChanged, stairNum, maskImg)
         plt.imshow(maskImg, cmap=plt.cm.gray)
         plt.draw()
--- step_detect.py
+++ step_detect.py
@@ -4,17 +4,25 @@ import ms_tools
 
 
 def getStepArr(masks, base, depth, depthThresh, filterPx, coverThresh):
+    # Pixels where depth has changed since the base image (where people are standing)
     changed = (base - depth) > depthThresh
+    # Remove thin-line noise from `changed`
     filteredChanged = ms_tools.thinLineFilter(changed, filterPx)
+    # Show pixels with changed depth as gray in the video
     vid = np.where(filteredChanged, 150, 0)
 
     numStairs = masks.shape[0]
     activeStairs = np.zeros(numStairs, np.dtype(bool))
+    # For each stair:
     for stairNum in range(0, numStairs):
         mask = masks[stairNum]
         area = np.sum(mask)
+        # Find where the mask overlaps with where people are currently standing
         coverageArr = np.logical_and(mask, filteredChanged)
+        # Area of this overlap
         covered = np.sum(coverageArr)
+        # If the area of the overlap is large enough (relative to the area of
+        # the mask), set the stair to active and show the mask in white on the video.
         if (float(covered) / float(area)) >= coverThresh:
             activeStairs[stairNum] = True
             vid = np.where(mask, 255, vid)
